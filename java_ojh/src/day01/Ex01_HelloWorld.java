e20b7661c02afd38e314122abff04361089597e0
irruyan <irruyan@gmail.com>
2024-12-30 12:38:26 +0900

    자바 1일차 헬로월드 예제

---
+++ java_ojh/src/day01/Ex01_HelloWorld.java
@@ -0,0 +1,19 @@
+package day01;
+/*
+ * 
+ */
+// 한줄
+
+public class Ex01_HelloWorld {
+	/**
+	 * 메소드 설명 주석
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		
+
+	}
+
+}
