500ad562469d49f898f557975da67a8e615524db
irruyan <irruyan@gmail.com>
2025-01-03 17:14:46 +0900

    숙제 250103

--- java_ojh/src/day04/Ex10_Homework.java
+++ java_ojh/src/day04/Ex10_Homework.java
@@ -1,5 +1,8 @@
 package day04;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex10_Homework {
 
 	public static void main(String[] args) {
@@ -15,7 +18,7 @@ public class Ex10_Homework {
 		 * 메뉴
 		 * 1. UpDown 게임 실행
 		 * 2. 최고 기록 확인
-		 * 3. 프로그램 종료		 * 
+		 * 3. 프로그램 종료
 		 * 메뉴선택 : 1
 		 * 입력 : 50
 		 * Down!
@@ -35,6 +38,33 @@ public class Ex10_Homework {
 		 * 메뉴선택 : 3
 		 * 프로그램을 종료합니다.
 		 */
+		
+		
+		Random random = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		int min = 0, max =100;
+		int r = random.nextInt(max - min + 1) + min;
+		
+		
+		
+		for( ; ; ) {
+			
+			System.out.print("숫자 입력 (1~100) : ");
+			int num = scan.nextInt();
+			
+			if(num < r) {
+				System.out.println("up");
+				}
+			else if(num > r){
+				System.out.println("down");				
+				}
+			else {
+				System.out.println("Correct!");
+				break;
+			}
+			
+		}
 	}
 
 }
